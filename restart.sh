34156046c1916d662d301c86a75e162d3443cf25
appar-sc <65882822+appar-sc@users.noreply.github.com>
2023-05-10 00:43:41 +0800

    build: updated restart script

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 docker stop chatgpt-web &&
 docker remove chatgpt-web &&
 docker build -t chatgpt-web . &&
-docker run --name chatgpt-web -d -p 139.162.107.239:3002:3002 --env OPENAI_API_KEY=$OPENAI_API_KEY chatgpt-web
+docker run --name chatgpt-web -d -p 139.162.107.239:3002:3002 --env OPENAI_API_KEY=$OPENAI_API_KEY --env OPENAI_API_MODEL=$OPENAI_API_MODEL chatgpt-web
